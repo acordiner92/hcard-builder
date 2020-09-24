398208c9cfc216e09ba99360fe4f020438578e98
Andrew Cordiner <andrewcordiner92@gmail.com>
2020-09-24 13:09:18 +1000

    Dabbled rendering the profile app view

--- src/ProfileController.ts
+++ src/ProfileController.ts
@@ -1,13 +1,60 @@
+/* eslint-disable no-var */
 import { Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
+import React from 'react';
+import ReactDomServer from 'react-dom/server';
 
-export const get = (request: Request, response: Response): Response => {
-  return response.send('get');
+// eslint-disable-next-line functional/prefer-type-literal
+interface Global {
+  // eslint-disable-next-line functional/prefer-readonly-type
+  React: any;
+}
+declare var global: Global;
+// eslint-disable-next-line functional/immutable-data
+global.React = React;
+
+export const get = (request: Request, response: Response): any => {
+  const hCardComponent = require('../view/main.js').default;
+  const hCardApp = ReactDomServer.renderToString(
+    React.createElement(hCardComponent, {
+      givenName: 'Sam',
+      surname: 'Fairfax',
+      email: 'sam.fairfax@fairfaxmedia.com.au',
+      phone: '0292822833',
+      houseNumber: '100',
+      street: 'Harris Street',
+      suburb: 'Pyrmont',
+      state: 'NSW',
+      postcode: '2009',
+      country: 'Australia',
+    }),
+  );
+  fs.readFile(
+    path.resolve(__dirname, '..', 'view/index.html'),
+    'utf-8',
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        return response.status(500).send('error has occured');
+      } else {
+        return response.send(
+          data.replace(
+            '<div class="HcardApp" />',
+            `<div class="HcardApp">${hCardApp}</div>`,
+          ),
+        );
+      }
+    },
+  );
 };
 
 export const submit = (request: Request, response: Response): Response => {
+  console.log('submit');
   return response.send('submit');
 };
 
 export const update = (request: Request, response: Response): Response => {
+  console.log('update');
   return response.send('update');
 };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import path from 'path';
 import { get, submit, update } from './ProfileController';
 
 const app = express();
 const router = express.Router();
 const port = 8080;
 
-router.use('/', get);
-router.use('/submit', submit);
-router.use('/update', update);
+router.use(express.static(path.resolve(__dirname, '..', 'view')));
+
+router.get('/', get);
+router.post('/submit', submit);
+router.post('/update', update);
 
 app.use(router);
 
