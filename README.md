b6b90783c0de1ac60fb651d22569037090bd01cb
Andrew Cordiner <andrewcordiner92@gmail.com>
2020-09-25 14:01:57 +1000

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # hcard-builder
+
+A profile creation application.
+
+## Requirements
+
+- NodeJS 14.3 or higher
+- docker
+
+## Installation
+
+- `npm i`
+- `docker-compose up (spin up postgres and redis)`
+- `npm run db:migration`
+- Create a profile_db_test database in postgres
+- `npm run db:migration:test`
+
+## How to run
+
+- `npm start`
+
+To simulate a SSR scenario you will need to update the shouldRenderSsr variable in ProfileViewController.ts
+By default it is turned off and SPA will be rendered.
+
+## How to run tests
+
+- `npm run test`
--- src/ProfileViewController.ts
+++ src/ProfileViewController.ts
@@ -2,6 +2,17 @@
 import { Request, Response } from 'express';
 import { GetPartialProfile, GetProfile } from './ProfileService';
 import { RenderSpa, RenderSsr } from './ProfileViewRenderer';
+
+// For this demo I am hardcoded this variable as a boolean
+// Ideally in a real life situation we would get a value out of
+// cookie to indicate whether the user has no js support or not.
+// https://www.codeproject.com/Tips/1217469/How-to-Detect-if-Client-has-JavaScript-Enabled-Dis
+const shouldRenderSsr = false;
+
+// Hardcoded user id here but ideally this would be retrieved by
+// the stateless auth setup like JWT's
+const userId = '2ab748b8-5b3c-4184-acb4-cb3550b8c6de';
+
 /**
  * Gets the profile view as SSR or SPA
  * depending if client has JS support or not.
@@ -15,17 +26,14 @@ export const getView = (
   getProfile: GetProfile,
   renderSsr: RenderSsr,
   renderSpa: RenderSpa,
-) => async (request: Request, response: Response): Promise<Response> => {
-  const shouldSsr = request.cookies && request.cookies['hasJs'] === 'false';
-  if (shouldSsr) {
-    const profile = await getProfile('2ab748b8-5b3c-4184-acb4-cb3550b8c6de');
+) => async (_request: Request, response: Response): Promise<Response> => {
+  if (shouldRenderSsr) {
+    const profile = await getProfile(userId);
     if (profile) {
       const view = await renderSsr({ ...profile });
       return response.send(view);
     } else {
-      const partialProfile = await getPartialProfile(
-        '2ab748b8-5b3c-4184-acb4-cb3550b8c6de',
-      );
+      const partialProfile = await getPartialProfile(userId);
       const view = await renderSsr(partialProfile);
       return response.send(view);
     }
